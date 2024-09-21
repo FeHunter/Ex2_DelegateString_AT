65c8a044222b2b2b4ac2909221d655b3cd729399
FeHunter <hunterfe1111@gmail.com>
2024-09-21 12:47:21 -0300

    Adicionar arquivos de projeto.

---
+++ Ex2_DelegateString_AT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex2_DelegateString_AT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 2 - AT");
+        }
+    }
+}
