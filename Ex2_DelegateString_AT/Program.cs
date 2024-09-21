cbb61e7cfc1e64b2e32085ad66d1284cccb58721
FeHunter <hunterfe1111@gmail.com>
2024-09-21 12:56:52 -0300

    Criado método anonimos e chamadas para as delegates

--- Ex2_DelegateString_AT/Program.cs
+++ Ex2_DelegateString_AT/Program.cs
@@ -4,9 +4,33 @@ namespace Ex2_DelegateString_AT
 {
     class Program
     {
+        public delegate void Frase (string frase);
+
+        static string LerFrase(string msg)
+        {
+            Console.Write(msg);
+            return Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Exercicio 2 - AT");
+
+            string frase = LerFrase("Digite uma frase: ");
+
+            // Declaração
+            Frase maiusculas = delegate (string frase)
+            {
+                Console.WriteLine($"Maiúsculas: {frase.ToUpper()}");
+            };
+
+            Frase minusculas = delegate (string frase)
+            {
+                Console.WriteLine($"Minúsculas: {frase.ToLower()}");
+            };
+
+            maiusculas(frase);
+            minusculas(frase);
         }
     }
 }
